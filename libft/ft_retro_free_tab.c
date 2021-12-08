2689f0d610b2f60859ddf8d90f1d6b3b308e0348
AlexDNZ <paleozene@gmail.com>
2021-12-08 16:31:03 +0100

    add libft new function

---
+++ libft/ft_retro_free_tab.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+void	ft_retro_free_tab(char **tab)
+{
+	int i;
+	
+	if (tab == NULL)
+		return ;
+	i = 0;
+	while (tab[i])
+		i++;
+	i++;
+	free(tab[i]);
+	while (--i >= 0)
+	{
+		if (tab[i])
+		{
+			free(tab[i]);
+			tab[i] = NULL;
+		}
+	}
+	free(tab);
+	tab = NULL;
+}
