2514f2ab2bd1b7b5361c2abce383bec9479fd3f8
Ivan LOISY <ivloisy@e1r13p13.42.fr>
2021-12-14 12:32:48 +0100

    pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ivloisy <ivloisy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 21:31:11 by ivloisy           #+#    #+#             */
-/*   Updated: 2021/12/13 21:51:09 by ivloisy          ###   ########.fr       */
+/*   Updated: 2021/12/14 12:29:11 by ivloisy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	pwd(void)
 {
-	char	*path;
-	char	s[4096];
+	char	*buf;
 
-	path = getcwd(s, 4096);
-	if (path == NULL)
+	buf = NULL;
+	buf = getcwd(buf, 0);
+	if (buf == NULL)
 	{
 		g_data.err = 1;
 		print_error();
 	}
 	else
 	{
-		write(1, path, ft_strlen(path));
+		write(1, buf, ft_strlen(buf));
 		write(1, "\n", 1);
 		g_data.err = 0;
 	}
