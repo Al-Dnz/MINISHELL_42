def2e651e1ff7b8fdc799d3c240142a1a3d2a28b
Ivan LOISY <ivloisy@e3r8p13.42.fr>
2021-12-15 22:30:37 +0100

    cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,12 +6,23 @@
 /*   By: ivloisy <ivloisy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 13:02:31 by ivloisy           #+#    #+#             */
-/*   Updated: 2021/12/15 20:42:02 by ivloisy          ###   ########.fr       */
+/*   Updated: 2021/12/15 22:28:02 by ivloisy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+/* 
+static int	check_nb(t_arg *arg)
+{
+	if (arg->next && arg->next->next)
+	{
+		g_data.err = 1;
+		write(2, "minishell: cd: too many arguments\n", 34);
+		return(0);
+	}
+	return (1);
+}
+ */
 static char	*set_path(t_arg *arg)
 {
 	char	*path;
@@ -49,14 +60,44 @@ static void	update_pwd(void)
 	}
 }
 
+static void	exec_cd(char *path)
+{
+	int		err;
+
+	if (path[0] == '-' && ft_strlen(path) > 1)
+	{
+		print_opt_error(path);
+		g_data.err = 1;
+		ft_strclr(&path);
+		return ;
+	}
+	err = chdir(path);
+	if (err == -1)
+	{
+		g_data.token_err = ft_strjoin("minishell: cd: ", path);//LEAKS
+		g_data.err = 1;
+		print_error();
+	}
+	else
+		update_pwd();
+	ft_strclr(&path);
+}
+
 void	cd(t_arg *arg)
 {
 	char	*path;
-	int		err;
 
+/* 	if (!check_nb(arg))
+		return ; */
+	if (arg->next && arg->next->next)
+	{
+		g_data.err = 1;
+		write(2, "minishell: cd: too many arguments\n", 34);
+		return ;
+	}
 	path = set_path(arg);
 	if (path)
-	{
+/* 	{
 		if (path[0] == '-' && ft_strlen(path) > 1)
 		{
 			print_opt_error(path);
@@ -74,5 +115,12 @@ void	cd(t_arg *arg)
 		else
 			update_pwd();
 		ft_strclr(&path);
+	} */
+		exec_cd(path);
+	else
+	{
+		g_data.err = 1;
+		g_data.token_err = ft_strdup("minishell");
+		print_error();
 	}
 }
