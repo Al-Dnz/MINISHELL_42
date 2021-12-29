775202239e19a1199c57bc36c2ab7e6b47b6a09e
Alexandre DENHEZ <adenhez@e2r6p18.clusters.42paris.fr>
2021-12-29 16:29:55 +0100

    fix fd 3

--- src/shell/print_error.c
+++ src/shell/print_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivloisy <ivloisy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: adenhez <adenhez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 00:39:07 by ivloisy           #+#    #+#             */
-/*   Updated: 2021/12/28 18:44:00 by ivloisy          ###   ########.fr       */
+/*   Updated: 2021/12/29 16:25:41 by adenhez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	print_error(void)
 {
 	if (g_data.status == 127)
 	{
-		write(2, g_data.token_err, ft_strlen(g_data.token_err));
-		write(2, ": No such file or directory\n", 28);
+		write(1, g_data.token_err, ft_strlen(g_data.token_err));
+		write(1, ": No such file or directory\n", 28);
 	}
 	if (g_data.status == 258)
 	{
-		write(2, "minishell: syntax error near unexpected token `", 47);
-		write(2, g_data.token_err, ft_strlen(g_data.token_err));
-		write(2, "'\n", 2);
+		write(1, "minishell: syntax error near unexpected token `", 47);
+		write(1, g_data.token_err, ft_strlen(g_data.token_err));
+		write(1, "'\n", 2);
 		ft_strclr(&g_data.token_err);
 	}
 	if (g_data.status == 1 && errno)
@@ -34,7 +34,7 @@ void	print_err_env(char *s)
 {
 	g_data.status = 1;
 	ft_putstr_fd("minishell: cd: ", 2);
-	ft_putstr_fd(s, 2);
+	ft_putstr_fd(s, 1);
 	ft_putstr_fd(" not set\n", 2);
 }
 
@@ -47,7 +47,7 @@ void	print_basic_error(void)
 
 void	print_opt_error(char *opt)
 {
-	write(2, "minishell: cd: ", 15);
-	write(2, opt, ft_strlen(opt));
-	write(2, ": invalid option\n", 17);
+	write(1, "minishell: cd: ", 15);
+	write(1, opt, ft_strlen(opt));
+	write(1, ": invalid option\n", 17);
 }
